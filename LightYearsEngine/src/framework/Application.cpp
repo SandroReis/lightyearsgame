1889d76cd035ad3ff0b5006e19a19bc4ebedf76e
SandroReis <sandrouh@hotmail.com>
2024-06-09 19:48:39 -0300

    feat: initial engine

---
+++ LightYearsEngine/src/framework/Application.cpp
@@ -0,0 +1,67 @@
+#include "framework/Application.h"
+#include "framework/Core.h"
+
+namespace ly
+{
+	Application::Application()
+		: mWindow{ sf::VideoMode(1024,1440), "Light Years" },
+		mTargetFramerate{ 60.f },
+		mTickClock{}
+	{
+		
+	}
+
+	void Application::Run()
+	{
+		mTickClock.restart();
+		float accumulatedTime = 0.f;
+		float targetDeltaTime = 1.f / mTargetFramerate;
+		while (mWindow.isOpen())
+		{
+			sf::Event windowEvent;
+			while (mWindow.pollEvent(windowEvent))
+			{
+				if (windowEvent.type == sf::Event::EventType::Closed)
+				{
+					mWindow.close();
+				}
+			}
+
+			accumulatedTime += mTickClock.restart().asSeconds();
+			while (accumulatedTime > targetDeltaTime)
+			{
+				accumulatedTime -= targetDeltaTime;
+				TickInternal(targetDeltaTime);
+				RenderInternal();
+			}
+		}
+	}
+	void Application::TickInternal(float deltaTime)
+	{
+		Tick(deltaTime);
+	}
+	void Application::RenderInternal()
+	{
+		mWindow.clear();
+
+		Render();
+
+
+		mWindow.display();
+	}
+	void Application::Render()
+	{
+		sf::RectangleShape rect{ sf::Vector2f{100,100} };
+		rect.setFillColor(sf::Color::Green);
+		rect.setOrigin(50, 50);
+
+		rect.setPosition(mWindow.getSize().x / 2.f, mWindow.getSize().y / 2.f);
+
+		mWindow.draw(rect);
+	}
+	void Application::Tick(float deltaTime)
+	{
+		LOG("ticking at framerate: %f", 1.f / deltaTime);
+	}
+}
+
