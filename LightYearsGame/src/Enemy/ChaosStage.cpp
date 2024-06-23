b160d6b20b667fadbf4b46f25148fb37e18613d8
SandroReis <sandrouh@hotmail.com>
2024-06-23 12:55:13 -0300

    fix: adjusting chaos stage

--- LightYearsGame/src/Enemy/ChaosStage.cpp
+++ LightYearsGame/src/Enemy/ChaosStage.cpp
@@ -3,60 +3,56 @@
 #include "Enemy/TwinBlade.h"
 #include "Enemy/UFO.h"
 #include "Enemy/Vanguard.h"
+
 #include "framework/MathUtility.h"
 #include "framework/World.h"
-
 namespace ly
 {
 	ChaosStage::ChaosStage(World* world)
 		: GameStage{ world },
 		mSpawnInterval{ 4.f },
-		mMinSpawnInterval{ .8f },
-		mSpawnIntervalDecrement{ .5f },
+		mMinSpawnInterval{ 0.8f },
+		mSpawnIntervalDecrement{ 0.5f },
 		mSpawnIntervalDecrementInterval{ 5.f },
 		mStageDuration{ 60.f }
 	{
-
 	}
+
 	void ChaosStage::StartStage()
 	{
 		mSpawnTimer = TimerManager::Get().SetTimer(GetWeakRef(), &ChaosStage::SpawnVanguard, mSpawnInterval);
 		mDifficultTimerHandle = TimerManager::Get().SetTimer(GetWeakRef(), &ChaosStage::IncreaseDifficulty, mSpawnIntervalDecrementInterval, true);
 		TimerManager::Get().SetTimer(GetWeakRef(), &ChaosStage::StageDurationReached, mStageDuration);
-
 	}
 
 	void ChaosStage::StageFinished()
 	{
 		TimerManager::Get().ClearTimer(mDifficultTimerHandle);
 		TimerManager::Get().ClearTimer(mSpawnTimer);
-
 	}
-
 	void ChaosStage::SpawnVanguard()
 	{
 		weak<Vanguard> newVanguard = GetWorld()->SpawnActor<Vanguard>();
 		newVanguard.lock()->SetActorLocation(GetRandomSpawnLocationTop());
 		mSpawnTimer = TimerManager::Get().SetTimer(GetWeakRef(), &ChaosStage::SpawnTwinBlade, mSpawnInterval);
 	}
-
 	void ChaosStage::SpawnTwinBlade()
 	{
-		weak<TwinBlade> newEnemy = GetWorld()->SpawnActor<TwinBlade>();
-		newEnemy.lock()->SetActorLocation(GetRandomSpawnLocationTop());
+		weak<TwinBlade> newTwinBlade = GetWorld()->SpawnActor<TwinBlade>();
+		newTwinBlade.lock()->SetActorLocation(GetRandomSpawnLocationTop());
 		mSpawnTimer = TimerManager::Get().SetTimer(GetWeakRef(), &ChaosStage::SpawnHexagon, mSpawnInterval);
 	}
 
 	void ChaosStage::SpawnHexagon()
 	{
-		weak<Hexagon> newEnemy = GetWorld()->SpawnActor<Hexagon>();
-		newEnemy.lock()->SetActorLocation(GetRandomSpawnLocationTop());
+		weak<Hexagon> newHexagon = GetWorld()->SpawnActor<Hexagon>();
+		newHexagon.lock()->SetActorLocation(GetRandomSpawnLocationTop());
 		mSpawnTimer = TimerManager::Get().SetTimer(GetWeakRef(), &ChaosStage::SpawnUFO, mSpawnInterval);
 	}
 
-
 	void ChaosStage::SpawnUFO()
 	{
+
 		sf::Vector2f spawnLoc = GetRandomSpawnLocationSide();
 
 		auto windowSize = GetWorld()->GetWindowSize();
@@ -64,47 +60,51 @@ namespace ly
 		sf::Vector2f dirToCenter = center - spawnLoc;
 		Normalize(dirToCenter);
 
-		weak<UFO> newEnemy = GetWorld()->SpawnActor<UFO>(dirToCenter * 200.f);
-		newEnemy.lock()->SetActorLocation(spawnLoc);
+		weak<UFO> newUFO = GetWorld()->SpawnActor<UFO>(dirToCenter * 200.f);
+		newUFO.lock()->SetActorLocation(spawnLoc);
 
 		mSpawnTimer = TimerManager::Get().SetTimer(GetWeakRef(), &ChaosStage::SpawnVanguard, mSpawnInterval);
 	}
+
 	void ChaosStage::IncreaseDifficulty()
 	{
-		if (mSpawnInterval == mMinSpawnInterval) return;
-
 		mSpawnInterval -= mSpawnIntervalDecrement;
-		if (mMinSpawnInterval < mMinSpawnInterval)
+		if (mSpawnInterval < mMinSpawnInterval)
 		{
 			mSpawnInterval = mMinSpawnInterval;
 		}
 	}
+
 	void ChaosStage::StageDurationReached()
 	{
 		FinishStage();
 	}
+
 	sf::Vector2f ChaosStage::GetRandomSpawnLocationTop() const
 	{
 		auto windowSize = GetWorld()->GetWindowSize();
 		float spawnX = RandomRange(100.f, windowSize.x - 100.f);
 		float spawnY = -100.f;
-		return { spawnX,spawnY };
+
+		return { spawnX, spawnY };
 	}
+
 	sf::Vector2f ChaosStage::GetRandomSpawnLocationSide() const
 	{
 		auto windowSize = GetWorld()->GetWindowSize();
 		float spawnLeft = RandomRange(-1, 1);
-		float spawnLocx = 0;
+		float spawnLocX = 0;
+
 		if (spawnLeft < 0)
 		{
-			spawnLocx = windowSize.x + 100.f;
+			spawnLocX = windowSize.x + 100.f;
 		}
 		else
 		{
-			spawnLocx = -100.f;
+			spawnLocX = -100.f;
 		}
 
 		float spawnLocY = RandomRange(0, windowSize.y);
-		return sf::Vector2f{ spawnLocx,spawnLocY };
+		return sf::Vector2f{ spawnLocX, spawnLocY };
 	}
 }
--- LightYearsGame/src/Enemy/VanguardStage.cpp
+++ LightYearsGame/src/Enemy/VanguardStage.cpp
@@ -8,15 +8,17 @@ namespace ly
 	VanguardStage::VanguardStage(World* world)
 		: GameStage{ world },
 		mSpawnInterval{ 1.5f },
-		mSwitchInterval{ 2.f },
+		mSwitchInterval{ 5.f },
 		mSpawnDistanceToEdge{ 100.f },
 		mLeftSpawnLoc{ 0.0f, 0.0f },
-		mSpawnLoc{ 0.0f, 0.0f },
+		mRightSpawnLoc{ 0.0f, 0.0f },
+		mSpawnLoc{ 0.0f,0.0f },
 		mRowsToSpawn{ 2 },
-		mCurrentRowVanguardCount{ 0 },
+		mRowSpawnCount{ 0 },
 		mVanguardsPerRow{ 5 },
-		mRowSpawnCount{ 0 }
+		mCurrentRowVanguardCount{ 0 }
 	{
+
 	}
 	void VanguardStage::StartStage()
 	{
